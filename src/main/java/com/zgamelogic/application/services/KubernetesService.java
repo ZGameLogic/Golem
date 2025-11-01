82a89e868bc749d51a89b91f6a7c6b45e786ca96
Ben Shabowski <benshabowski@gmail.com>
2025-11-01 15:15:45 -0500

    Enabling scheduling

--- src/main/java/com/zgamelogic/application/services/KubernetesService.java
+++ src/main/java/com/zgamelogic/application/services/KubernetesService.java
@@ -5,9 +5,11 @@ import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class KubernetesService {
 
@@ -16,11 +18,11 @@ public class KubernetesService {
     public void init() {
         KubernetesClient client = new KubernetesClientBuilder().build();
         PodList podList = client.pods().inNamespace("default").list();
-        System.out.println("=== Current Pods in Cluster ===");
+        log.info("=== Current Pods in Cluster ===");
         for (Pod pod : podList.getItems()) {
             String name = pod.getMetadata().getName();
             String phase = pod.getStatus().getPhase();
-            System.out.printf("%-38s %s%n", name, phase);
+            log.info("{} {}", name, phase);
         }
     }
 }
