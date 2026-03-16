7f829a49c3faae3b19e8bc8893334ca7272e201e
zabory <benshabowski@gmail.com>
2026-03-15 20:42:51 -0500

    update project configuration to use JDK 25 and update dependencies

--- src/main/java/com/zgamelogic/application/kubernetes/KubernetesService.java
+++ src/main/java/com/zgamelogic/application/kubernetes/KubernetesService.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 
 @Slf4j
